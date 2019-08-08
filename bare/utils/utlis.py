4f5024a816b848e4e9cfa97901bb65cc8ef18424
Friedrich Knuth <knuth@uw.edu>
2019-08-08 14:52:31 -0700

    add check if ovr exists

--- bare/utils/utlis.py
+++ bare/utils/utlis.py
@@ -1,10 +1,13 @@
 import subprocess
 import sys
+import os
 from distutils.spawn import find_executable
 import multiprocessing
 from functools import partial
 
 def create_overview(img_file_name, scale=8):
+    # TODO
+    # add check if overview already exists
     '''
     Function to generate overviews using gdaladdo. gdal must be installed to environment.
     Default overview scale is 8. Can specify multiple overview scales at once, e.g. scale = 8 16 32 64.
@@ -14,18 +17,25 @@ def create_overview(img_file_name, scale=8):
         print('gdaladdo command not found. Please make sure gdal is installed to your environment. Cannot generate overview.')
         sys.exit(1)
     
-    # build call
-    call = "gdaladdo -ro -r average --config COMPRESS_OVERVIEW LZW --config BIGTIFF_OVERVIEW YES".split()
-    call.append(img_file_name)
-    call.append(str(scale))
+    overview_file = img_file_name+'.ovr'
     
-    # run command and print to standard out
-    return subprocess.check_output(call)
+    if os.path.isfile(overview_file):
+        print(overview_file, 'already exists.')
+        return
+        
+    else:
+        # build call
+        call = "gdaladdo -ro -r average --config COMPRESS_OVERVIEW LZW --config BIGTIFF_OVERVIEW YES".split()
+        call.append(img_file_name)
+        call.append(str(scale))
+    
+        # run command and print to standard out
+        subprocess.check_output(call)
 
 def parallel_create_overview(image_list, scale=8, threads=8):
     # TODO
-    # use threading.active_count() or os.cpu_count() to determine optimal thread count based on machine,
-    # if possible.
+    # use os.cpu_count() to determine optimal thread count based on machine,
+    # if reasonable approach.
     '''
     Function to run create_overview() in parallel. Threads set to 8 by default.
     '''
